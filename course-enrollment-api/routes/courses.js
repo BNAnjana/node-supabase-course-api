d4f3b051766d2e6d578531cd0cf0f2c1254ab7cd
BNAnjana <nagaanjana003@gmail.com>
2026-03-08 17:26:18 +0530

    fifth commit

--- course-enrollment-api/routes/courses.js
+++ course-enrollment-api/routes/courses.js
@@ -5,7 +5,7 @@ import validateEnrollment from '../middleware/validateEnrollment.js';
 
 router.post('/courses', async (req, res) => {
   const { id, title, instructor } = req.body;
-  const { data, error } = await supabase('courses')
+  const { data, error } = await supabase.from('courses')
     .insert([{ id, title, instructor }]);
 
   if (error) return res.status(500).json({ error: error.message });
@@ -14,7 +14,7 @@ router.post('/courses', async (req, res) => {
 
 // 1. Get All Courses
 router.get('/courses', async (req, res) => {
-  const { data, error } = await supabase('courses').select('*');
+  const { data, error } = await supabase.from('courses').select('*');
   if (error) return res.status(500).json({ error: error.message });
   res.json(data);
 });
@@ -22,7 +22,7 @@ router.get('/courses', async (req, res) => {
 // 2. Enroll a Student
 router.post('/enroll', validateEnrollment, async (req, res) => {
   const { student_name, course_id } = req.body;
-  const { data, error } = await supabase('enrollments')
+  const { data, error } = await supabase.from('enrollments')
     .insert([{ student_name, course_id }]);
 
   if (error) return res.status(500).json({ error: error.message });
@@ -32,7 +32,7 @@ router.post('/enroll', validateEnrollment, async (req, res) => {
 // 3. Get Enrollments for a Course
 router.get('/courses/:id/enrollments', async (req, res) => {
   const courseId = req.params.id;
-  const { data, error } = await supabase('enrollments')
+  const { data, error } = await supabase.from('enrollments')
     .select('student_name, course_id')
     .eq('course_id', courseId);
 
