7873f2773dc82101c3e223b668d3246491ff5ea7
BNAnjana <nagaanjana003@gmail.com>
2026-03-08 15:14:33 +0530

    second commit

--- course-enrollment-api/routes/courses.js
+++ course-enrollment-api/routes/courses.js
@@ -1,2 +1,35 @@
-import { supabase } from "./supabaseClient.js";
+import { Router } from 'express';
+const router = Router();
+import supabase from '../supabaseClient.js';
+import validateEnrollment from '../middleware/validateEnrollment.js';
+
+// 1. Get All Courses
+router.get('/courses', async (req, res) => {
+  const { data, error } = await supabase('courses').select('*');
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(data);
+});
+
+// 2. Enroll a Student
+router.post('/enroll', validateEnrollment, async (req, res) => {
+  const { student_name, course_id } = req.body;
+  const { data, error } = await supabase('enrollments')
+    .insert([{ student_name, course_id }]);
+
+  if (error) return res.status(500).json({ error: error.message });
+  res.status(201).json(data);
+});
+
+// 3. Get Enrollments for a Course
+router.get('/courses/:id/enrollments', async (req, res) => {
+  const courseId = req.params.id;
+  const { data, error } = await supabase('enrollments')
+    .select('student_name, course_id')
+    .eq('course_id', courseId);
+
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(data);
+});
+
+export default router;
 
--- course-enrollment-api/server.js
+++ course-enrollment-api/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import router from "./routes/courses.js";
+import logger from"./middleware/logger.js";
 
 const app = express();
 app.use(express.json());
+
+app.use(logger);
+app.use('/', router);
 
 const PORT = process.env.PORT || 3000;
 
